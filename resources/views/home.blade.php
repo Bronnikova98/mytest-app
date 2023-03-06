192f04b856baa8d0b9b22c92b8332ef9e2d91da3
Bronnikova98 <bronnikowa-wika@mail.ru>
2023-03-06 23:00:52 +0500

    Added output of articles on the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    
+    <div>         
+        <div class="m-4">
+            <h1>Статьи</h1></div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        @foreach ($posts_title as $ptitle)
+            <div class="m-4">
+
+                <div class="card bg-light">
+
+                    <div class="card-body pt-0 m-4">
+                        <div class="row">
+                            <div class="col-8">
+                                <h4 class="text-muted text-sm"><b>{{ $ptitle['title'] }}</b></h4>
+                                <p class="text-muted text-sm">{{ $ptitle['text'] }}</p>
+
+                            </div>
+                            <div class="col-4">
+                                <img src="{{ $ptitle['img'] }}" alt="" class="img-circle img-fluid">
+                            </div>
                         </div>
-                    @endif
+                    </div>
 
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
-        </div>
+
+
+
+
     </div>
-</div>
+
+    
+    @endforeach
+    
 @endsection
